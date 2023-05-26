cac92913b4bcde0903ae187de992da0f7a9ab514
igormaxlima <igormaxnunes@gmail.com>
2023-05-26 20:14:42 -0300

    Corrigindo bug de dependencias

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,14 +1,17 @@
-import getQrCode from '../../helpers/getQrCode'
 import './styles.css'
+import QRCode from "react-qr-code";  
+
 
 function App() {
 
   return (
     <div className="App"> 
       <h1>Cardapio de Cliente</h1>
+      <QRCode value='www.google.com'/>
     </div>
 
   );
 }
+
 
 export default App
